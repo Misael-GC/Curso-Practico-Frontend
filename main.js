ce19a78d83b028cdfdd07f03c269c4c5eebefd72
Misael-GC <misael.gomez1623@gmail.com>
2022-08-09 16:49:02 -0500

    menu, mobile and desktop

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+    const menuEmail = document.querySelector('.navbar-email');
+    const desktopMenu = document.querySelector('.desktop-menu');
+    const menuHamIcon = document.querySelector('.menu')
+    const mobileMenu = document.querySelector('.mobile-menu')
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+    menuEmail.addEventListener('click', toggleDesktopMenu);
+    menuHamIcon.addEventListener('click', toggleMobileMenu);
 
-function toggleDesktopMenu(){
-    const toggle = desktopMenu.classList.toggle('inactive');
-    return toggle;
-}
+    function toggleDesktopMenu(){
+        const toggleDesktop = desktopMenu.classList.toggle('inactive');
+        return toggleDesktop;
+    };
+
+    function toggleMobileMenu(){
+        const toggleModile = mobileMenu.classList.toggle('inactive');
+        return toggleMobileMenu;
+    }
