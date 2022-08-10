31885bc48af535ea2482bdb1aaaa3fb2c293513e
Misael-GC <misael.gomez1623@gmail.com>
2022-08-10 13:16:01 -0500

    sección de productos contruido desde JavaScript, conectandolo con html

---
+++ product.js
@@ -0,0 +1,17 @@
+const productList = [
+    {
+        name: 'Bike',
+        price: 120,
+        image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    },
+    {
+        name: 'Pantalla',
+        price: 220,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      },
+      {
+        name: 'Compu',
+        price: 620,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      }
+];
