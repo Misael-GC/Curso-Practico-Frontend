cea5a6cdfffe659a5ba64458091506397540e73b
Misael-GC <misael.gomez1623@gmail.com>
2022-08-09 20:18:39 -0500

    agregando el carrito de compras,, creando condicionales para que las tarjetas no se encimen unas sobre otras al hacer click, cuando una abrá otra cierre

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
     const menuEmail = document.querySelector('.navbar-email');
     const desktopMenu = document.querySelector('.desktop-menu');
-    const menuHamIcon = document.querySelector('.menu')
-    const mobileMenu = document.querySelector('.mobile-menu')
+    const menuHamIcon = document.querySelector('.menu');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+    const aside = document.querySelector('.product-detail');
 
     menuEmail.addEventListener('click', toggleDesktopMenu);
     menuHamIcon.addEventListener('click', toggleMobileMenu);
+    menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
     function toggleDesktopMenu(){
-        const toggleDesktop = desktopMenu.classList.toggle('inactive');
-        return toggleDesktop;
-    };
+        const isASideClosed = aside.classList.contains('inactive');
+
+        if(!isASideClosed){ //si está abierto
+            aside.classList.add('inactive');//cierralo
+        }
+
+        desktopMenu.classList.toggle('inactive');
+        };
 
     function toggleMobileMenu(){
-        const toggleModile = mobileMenu.classList.toggle('inactive');
-        return toggleMobileMenu;
+        const isASideClosed = aside.classList.contains('inactive');
+
+        if(!isASideClosed){ //si está abierto
+            aside.classList.add('inactive');//cierralo
+        }
+
+        mobileMenu.classList.toggle('inactive');
+    }
+
+    function toggleCarritoAside(){
+        const isMobileMenuClosed = mobileMenu.classList.contains('inactive'); //contains en las notas es un true o false, check it
+
+        if(!isMobileMenuClosed){ //si está abierto
+            mobileMenu.classList.add('inactive'); //cierralo
+        }
+
+        //aqui va lo del menu desktop
+        const isDesktopMenu = desktopMenu.classList.contains('inactive');
+
+        if(!isDesktopMenu){
+            desktopMenu.classList.add('inactive');
+        }//aqui termina si en algun momento hay que delete it
+
+        aside.classList.toggle('inactive');
     }
